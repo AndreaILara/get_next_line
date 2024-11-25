123bf73f32464bc69d64f187c697c3013bf5cad2
Andrea <andreainigolara@hotmail.com>
2024-11-25 16:42:14 +0100

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -3,9 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ainigo-l <ainigo-l@student.42madrid.com>   #+#  +:+       +#+        */
+/*   By: ainigo-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-10-09 11:49:28 by ainigo-l          #+#    #+#             */
-/*   Updated: 2024-10-09 11:49:28 by ainigo-l         ###   ########.fr       */
+/*   Created: 2024/11/25 16:35:32 by ainigo-l          #+#    #+#             */
+/*   Updated: 2024/11/25 16:35:35 by ainigo-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*ft_freeall(char **s);
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *dst, char *src);
+char	*ft_substr(char *s, int start, int size);
+char	*ft_strchr(char *s, int c);
+
+#endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,9 +3,92 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ainigo-l <ainigo-l@student.42madrid.com>   #+#  +:+       +#+        */
+/*   By: ainigo-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-10-09 11:49:23 by ainigo-l          #+#    #+#             */
-/*   Updated: 2024-10-09 11:49:23 by ainigo-l         ###   ########.fr       */
+/*   Created: 2024/11/25 16:35:47 by ainigo-l          #+#    #+#             */
+/*   Updated: 2024/11/25 16:35:50 by ainigo-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *dst, char *src)
+{
+	char	*result;
+	size_t	i;
+	size_t	j;
+
+	if (!dst)
+	{
+		dst = malloc(1);
+		if (!dst)
+			return (NULL);
+		dst[0] = '\0';
+	}
+	if (!src)
+		return (NULL);
+	i = -1;
+	j = -1;
+	result = malloc((ft_strlen(dst) + ft_strlen(src) + 1) * sizeof(char));
+	if (!result)
+		return (ft_freeall(&dst));
+	while (dst[++i])
+		result[i] = dst[i];
+	while (src[++j])
+		result[i++] = src[j];
+	result[i] = '\0';
+	free(dst);
+	return (result);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_substr(char *s, int start, int size)
+{
+	int		i;
+	int		slen;
+	char	*newstr;
+
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if ((slen - start) < size)
+		size = slen - start;
+	if (start >= (int)ft_strlen(s))
+		return ("");
+	i = 0;
+	newstr = malloc((size + 1) * sizeof(char));
+	if (!newstr)
+		return (NULL);
+	while (s[start + i] && i < size)
+	{
+		newstr[i] = s[start + i];
+		i++;
+	}
+	newstr[i] = '\0';
+	return (newstr);
+}
